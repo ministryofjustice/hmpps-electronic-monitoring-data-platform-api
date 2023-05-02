209cac6f14b54b190218a4d5c360c7a747c2d497
ben-cart3r <ben-cart3r@users.noreply.github.com>
2023-05-02 17:17:52 +0000

    updating project name after deployment from template repository

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/HmppsElectronicMonitoringDataPlatformApi.kt
@@ -0,0 +1,11 @@
+package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication()
+class HmppsElectronicMonitoringDataPlatformApi
+
+fun main(args: Array<String>) {
+  runApplication<HmppsElectronicMonitoringDataPlatformApi>(*args)
+}
