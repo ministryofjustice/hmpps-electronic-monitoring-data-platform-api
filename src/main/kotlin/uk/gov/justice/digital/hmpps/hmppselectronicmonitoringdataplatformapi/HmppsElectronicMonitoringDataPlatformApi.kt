a5b3da8ad1405ceb0e4a9c0e8f03a2a2400b015a
Jasper <jasper.jackson@madetech.com>
2023-05-09 11:45:12 +0100

    Auto-ordering of imports disagrees with circleCI lint guide

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/HmppsElectronicMonitoringDataPlatformApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/HmppsElectronicMonitoringDataPlatformApi.kt
@@ -1,19 +1,9 @@
 package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi
-
-// import org.springframework.boot.autoconfigure.SpringBootApplication
-// import org.springframework.boot.runApplication
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.plugins.configureRouting
 import io.ktor.server.application.Application
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import java.util.*
-import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.plugins.configureRouting
-
-// @SpringBootApplication()
-// class HmppsElectronicMonitoringDataPlatformApi
-//
-// fun main(args: Array<String>) {
-//  runApplication<HmppsElectronicMonitoringDataPlatformApi>(*args)
-// }
 
 fun main() {
   embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
