7920e6f4ea665237ccb7655af69ad720e5894591
pedro.soares <pedro.soares@justice.gov.uk>
2023-05-19 09:52:53 +0100

    clean codebase

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/classes/User.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/classes/User.kt
@@ -1,4 +1,3 @@
 package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.classes
 
-data class User(val id: Int? = null, val name: String, val age: Int) {
-}
+data class User(val id: Int? = null, val name: String, val age: Int)
