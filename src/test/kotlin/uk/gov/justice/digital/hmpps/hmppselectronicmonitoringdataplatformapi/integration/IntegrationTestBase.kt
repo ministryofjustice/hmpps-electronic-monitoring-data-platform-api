f3a16edc6916af884f8b26e335d9fb18cbad8ead
mandanakhademi <mandanakhademi88@gmail.com>
2023-05-25 18:43:37 +0100

    Fix gradle feedback

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/integration/IntegrationTestBase.kt
@@ -5,11 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
-import org.testcontainers.containers.JdbcDatabaseContainer
-import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
-import org.testcontainers.utility.DockerImageName
 import java.time.Duration
 
 @SpringBootTest(
@@ -30,14 +26,14 @@ abstract class IntegrationTestBase {
       .build()
   }
 
-  companion object {
-    @Container
-    val container = postgres("postgres:15.3") {
-      withDatabaseName("test_db")
-      withUsername("postgres")
-      withPassword("root")
-    }
-
+//  companion object {
+//    @Container
+//    val container = postgres("postgres:15.3") {
+//      withDatabaseName("test_db")
+//      withUsername("postgres")
+//      withPassword("root")
+//    }
+//
 //    @JvmStatic
 //    @DynamicPropertySource
 //    fun datasourceConfig(registry: DynamicPropertyRegistry) {
@@ -45,8 +41,8 @@ abstract class IntegrationTestBase {
 //      registry.add("spring.datasource.password", container::getPassword)
 //      registry.add("spring.datasource.username", container::getUsername)
 //    }
-  }
+//  }
 }
 
-fun postgres(imageName: String, opts: JdbcDatabaseContainer<Nothing>.() -> Unit) =
-  PostgreSQLContainer<Nothing>(DockerImageName.parse(imageName)).apply(opts)
+// fun postgres(imageName: String, opts: JdbcDatabaseContainer<Nothing>.() -> Unit) =
+//  PostgreSQLContainer<Nothing>(DockerImageName.parse(imageName)).apply(opts)
