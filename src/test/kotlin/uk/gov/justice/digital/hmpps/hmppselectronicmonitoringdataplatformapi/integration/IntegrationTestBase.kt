135b052462f2cf3b5246c606a261d588bf8fc60e
mandanakhademi <mandanakhademi88@gmail.com>
2023-05-25 17:31:57 +0100

    Fix gradle feedback

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/integration/IntegrationTestBase.kt
@@ -1,13 +1,16 @@
 package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.integration
 
+import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.testcontainers.containers.JdbcDatabaseContainer
 import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
+import java.time.Duration
 
 @SpringBootTest(
   webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
@@ -19,13 +22,21 @@ abstract class IntegrationTestBase {
   @Autowired
   lateinit var webTestClient: WebTestClient
 
-//  companion object {
-//    @Container
-//    val container = postgres("postgres:15.3") {
-//      withDatabaseName("test_db")
-//      withUsername("postgres")
-//      withPassword("root")
-//    }
+  @BeforeEach
+  fun setUp() {
+    webTestClient = webTestClient
+      .mutate()
+      .responseTimeout(Duration.ofMillis(30000))
+      .build()
+  }
+
+  companion object {
+    @Container
+    val container = postgres("postgres:15.3") {
+      withDatabaseName("test_db")
+      withUsername("postgres")
+      withPassword("root")
+    }
 
 //    @JvmStatic
 //    @DynamicPropertySource
@@ -34,7 +45,7 @@ abstract class IntegrationTestBase {
 //      registry.add("spring.datasource.password", container::getPassword)
 //      registry.add("spring.datasource.username", container::getUsername)
 //    }
-//  }
+  }
 }
 
 fun postgres(imageName: String, opts: JdbcDatabaseContainer<Nothing>.() -> Unit) =
