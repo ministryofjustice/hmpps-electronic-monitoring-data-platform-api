176569299ad1e2e7dd339e7e909d190d0c5f4cb1
mandanakhademi <mandanakhademi88@gmail.com>
2023-05-25 13:56:33 +0100

    Fix gradle feedback

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/integration/IntegrationTestBase.kt
@@ -1,17 +1,16 @@
 package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.integration
 
+import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.DynamicPropertyRegistry
-import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.testcontainers.containers.JdbcDatabaseContainer
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
+import java.time.Duration
 
 @SpringBootTest(
   webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
@@ -23,8 +22,13 @@ abstract class IntegrationTestBase {
   @Autowired
   lateinit var webTestClient: WebTestClient
 
-  @Autowired
-  lateinit var jdbc: JdbcTemplate
+  @BeforeEach
+  fun setUp() {
+    webTestClient = webTestClient
+      .mutate()
+      .responseTimeout(Duration.ofMillis(30000))
+      .build()
+  }
 
   companion object {
     @Container
@@ -34,16 +38,15 @@ abstract class IntegrationTestBase {
       withPassword("root")
     }
 
-    @JvmStatic
-    @DynamicPropertySource
-    fun datasourceConfig(registry: DynamicPropertyRegistry) {
-      registry.add("spring.datasource.url", container::getJdbcUrl)
-      registry.add("spring.datasource.password", container::getPassword)
-      registry.add("spring.datasource.username", container::getUsername)
-    }
+//    @JvmStatic
+//    @DynamicPropertySource
+//    fun datasourceConfig(registry: DynamicPropertyRegistry) {
+//      registry.add("spring.datasource.url", container::getJdbcUrl)
+//      registry.add("spring.datasource.password", container::getPassword)
+//      registry.add("spring.datasource.username", container::getUsername)
+//    }
   }
 }
-
 
 fun postgres(imageName: String, opts: JdbcDatabaseContainer<Nothing>.() -> Unit) =
   PostgreSQLContainer<Nothing>(DockerImageName.parse(imageName)).apply(opts)
