06a53e02bdd1bc92415f2f4085362ea2e669ee79
Jasper <54336703+JazJax@users.noreply.github.com>
2023-06-06 12:01:11 +0100

    Reverting all changes since updating from template (#19)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/HmppsElectronicMonitoringDataPlatformApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/HmppsElectronicMonitoringDataPlatformApi.kt
@@ -1,33 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi
 
-import io.ktor.server.application.Application
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
-import org.koin.core.context.GlobalContext.startKoin
-import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.plugins.appModule
-import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.plugins.configureDatabases
-import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.plugins.configureRouting
-import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.plugins.configureSerialization
-import java.util.*
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
-fun main() {
-  startKoin {
-    modules(appModule)
-  }
-  embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-    .start(wait = true)
-  val properties = Properties()
-  properties.load(Application::class.java.getResourceAsStream("/version.properties"))
-  println(properties.getProperty("version"))
-}
+@SpringBootApplication()
+class HmppsElectronicMonitoringDataPlatformApi
 
-fun Application.module() {
-  try {
-    configureDatabases()
-  } catch (e: Exception) {
-    // this will need to be changed to logging
-    println(e.message)
-  }
-  configureRouting()
-  configureSerialization()
+fun main(args: Array<String>) {
+  runApplication<HmppsElectronicMonitoringDataPlatformApi>(*args)
 }
