7a008ca5719b22d488a950e60252a9a8da8cfadc
Jasper <54336703+JazJax@users.noreply.github.com>
2023-06-06 12:01:11 +0100

    Reverting all changes since updating from template (#19)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/classes/User.kt
+++
@@ -1,3 +0,0 @@
-package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.classes
-
-data class User(val id: Int? = null, val name: String, val age: Int)
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/services/DatabaseService.kt
+++
@@ -1,7 +0,0 @@
-package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.services
-
-import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.repositories.DatabaseRepository
-
-class DatabaseService(private val databaseRepository: DatabaseRepository) {
-  fun initializeDatabase(): Unit = databaseRepository.initializeDatabase()
-}
