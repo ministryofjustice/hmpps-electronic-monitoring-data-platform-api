86703adbd44019cd8ceda599ddc023af7c57e80e
mandanakhademi <mandanakhademi88@gmail.com>
2023-06-13 14:09:38 +0100

    Update repository interface to support find by a specific field

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/repository/DeviceWearerRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/repository/DeviceWearerRepository.kt
@@ -1,6 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.repository
 
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.model.DeviceWearer
 
-interface DeviceWearerRepository : CrudRepository<DeviceWearer, Int>
+@Repository
+interface DeviceWearerRepository : JpaRepository<DeviceWearer, Int> {
+  fun findByDeviceWearerId(id: String): DeviceWearer?
+
+}
