56bef8c47ebbb6ac493007b26201b5a70a9fba95
Ben Carter <ben.carter@justice.gov.uk>
2023-06-13 17:37:44 +0100

    Seed some device_wearer data on application startup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+TRUNCATE TABLE device_wearer;
+
+INSERT INTO device_wearer (id, device_wearer_id, first_name, last_name, "type") 
+VALUES 
+    (1, '123456789', 'John', 'Smith', 'Historical Case Centric'), 
+    (2, '987654321', 'Jane', 'Doe', 'Historical User Centric')
+;
