6bda49b29cea33ca6bbadbb2199211aef494814b
jasper.jackson <jasper.jackson@madetech.com>
2023-06-15 15:56:48 +0100

    Fixed failing integration tests with compatibility shim.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/integration/health/InfoTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/integration/health/InfoTest.kt
@@ -2,11 +2,11 @@ package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.in
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.integration.IntegrationTestBaseCompatibility
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-class InfoTest : IntegrationTestBase() {
+class InfoTest : IntegrationTestBaseCompatibility() {
 
   @Test
   fun `Info page is accessible`() {
