7d7aa9f73898d87e7316862d0379c2d511987b05
Jasper <54336703+JazJax@users.noreply.github.com>
2023-06-26 16:51:28 +0100

    Search now runs SQL queries on the db instead of pulling and filtering all records (#22)
    * Updated table name
    * search v2 searches the database via SQL
    ---------
    Co-authored-by: pedro.soares <pedro.soares@justice.gov.uk>
    Co-authored-by: mandanakhademi <mandanakhademi88@gmail.com>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/responses/DeviceWearerResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/responses/DeviceWearerResponse.kt
@@ -1,18 +1,15 @@
 package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.responses
-
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.model.BaseResponse
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.model.DeviceWearer
-
 class DeviceWearerResponse : BaseResponse {
   val deviceWearers: List<DeviceWearer>
-  constructor(deviceWearerList: List<DeviceWearer>, error: String = "") : super(error) {
-    this.deviceWearers = deviceWearerList
+  constructor(deviceWearerList: List<DeviceWearer>?, error: String = "") : super(error) {
+    this.deviceWearers = deviceWearerList ?:listOf()
   }
   constructor(deviceWearer: DeviceWearer, error: String = "") : super(error) {
     this.deviceWearers = listOf<DeviceWearer>(deviceWearer)
   }
   constructor(error: String) : super(error) {
     this.deviceWearers = listOf<DeviceWearer>()
-    this.error = error
   }
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/service/DeviceWearerService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/service/DeviceWearerService.kt
@@ -1,34 +1,31 @@
 package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.service
-
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.model.DeviceWearer
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.repository.DeviceWearerRepository
-
 interface IDeviceWearerService {
   fun getAllDeviceWearers(): List<DeviceWearer>
+  fun getMatchingDeviceWearers(parameter : String): List<DeviceWearer>?
   fun getDeviceWearerById(id: String): DeviceWearer?
   fun createDummyDeviceWearers(): List<DeviceWearer>
 }
-
 @Service
 class DeviceWearerService(@Autowired private val deviceWearerRepository: DeviceWearerRepository) :
   IDeviceWearerService {
   override fun getAllDeviceWearers(): List<DeviceWearer> {
     return deviceWearerRepository.findAll().toList()
   }
-
+  override fun getMatchingDeviceWearers(parameter : String): List<DeviceWearer>? {
+    return deviceWearerRepository.searchDatabase(parameter)
+  }
   override fun getDeviceWearerById(id: String): DeviceWearer? {
     return deviceWearerRepository.findByDeviceWearerId(id)
   }
-
   override fun createDummyDeviceWearers(): List<DeviceWearer> {
     val deviceWearer1 = DeviceWearer(deviceWearerId = "abc123UID", firstName = "Billiam", lastName = "Doors", type = "Dummy")
     deviceWearerRepository.save(deviceWearer1)
-
     val deviceWearer2 = DeviceWearer(deviceWearerId = "UID456784", firstName = "John", lastName = "Smith", type = "Dummy")
     deviceWearerRepository.save(deviceWearer2)
-
     return deviceWearerRepository.findAll().toList()
   }
 }
