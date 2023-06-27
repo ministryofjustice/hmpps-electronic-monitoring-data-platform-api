52e212f8940520145cfd4b34cdcfe49d81323c1c
mandanakhademi <mandanakhademi88@gmail.com>
2023-06-27 12:48:21 +0100

    Add test for getMatchingDeviceWearers

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/service/DeviceWearerServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/service/DeviceWearerServiceTest.kt
@@ -3,13 +3,15 @@ package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.se
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.model.DeviceWearer
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.repository.DeviceWearerRepository
 
 class DeviceWearerServiceTest {
+  val deviceWearerRepository = Mockito.mock(DeviceWearerRepository::class.java)
   @Test
   fun `getAllDeviceWearers should return an empty list when there is no device wearer`() {
-    val deviceWearerRepository = Mockito.mock(DeviceWearerRepository::class.java)
     val expected = listOf<DeviceWearer>()
     Mockito.`when`(deviceWearerRepository.findAll().toList()).thenReturn(expected)
     val deviceWearerService = DeviceWearerService(deviceWearerRepository)
@@ -19,7 +21,6 @@ class DeviceWearerServiceTest {
 
   @Test
   fun `getAllDeviceWearers should return a list of items when there are some device wearers`() {
-    val deviceWearerRepository = Mockito.mock(DeviceWearerRepository::class.java)
     val expected = listOf<DeviceWearer>(
       DeviceWearer(1, "1234", "John", "Smith", "Curfew"),
       DeviceWearer(2, "5678", "Oliver", "Brown", "Inclusion Zone"),
@@ -32,7 +33,6 @@ class DeviceWearerServiceTest {
 
   @Test
   fun `getDeviceWearerById should return null when device wearer does not exist`() {
-    val deviceWearerRepository = Mockito.mock(DeviceWearerRepository::class.java)
     val expected = null
     val id = "345ab"
     Mockito.`when`(deviceWearerRepository.findByDeviceWearerId(id)).thenReturn(expected)
@@ -43,12 +43,19 @@ class DeviceWearerServiceTest {
 
   @Test
   fun `getDeviceWearerById should return an item when device wearer exists`() {
-    val deviceWearerRepository = Mockito.mock(DeviceWearerRepository::class.java)
     val expected = DeviceWearer(1, "1234", "John", "Smith", "Curfew")
     val id = "345ab"
     Mockito.`when`(deviceWearerRepository.findByDeviceWearerId(id)).thenReturn(expected)
     val deviceWearerService = DeviceWearerService(deviceWearerRepository)
     val actual = deviceWearerService.getDeviceWearerById(id)
     Assertions.assertThat(actual).isEqualTo(expected)
+  }
+
+  @Test
+  fun `getMatchingDeviceWearers should search the database with a supplied parameter`() {
+    val param = "etdgh34"
+    val deviceWearerService = DeviceWearerService(deviceWearerRepository)
+    deviceWearerService.getMatchingDeviceWearers(param)
+    verify(deviceWearerRepository, times(1)).searchDatabase(param)
   }
 }
