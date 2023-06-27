63f411fa959963a44289414345f39ea713476eab
jasper.jackson <jasper.jackson@madetech.com>
2023-06-27 16:23:17 +0100

    Added function to get GPS data by user ID

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/repository/GPSDataRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/repository/GPSDataRepository.kt
@@ -1,7 +1,15 @@
 package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.repository
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.model.GPSData
-import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.model.User
 
-interface GPSDataRepository : CrudRepository<GPSData, Int>
+interface GPSDataRepository : CrudRepository<GPSData, Int> {
+
+  @Query(
+    value = "SELECT * FROM device_wearer u WHERE u.first_name = 'Jane'",
+    nativeQuery = true,
+  )
+  fun findByDeviceWearerId(@Param("deviceWearerId") deviceWearerId: String): List<GPSData>?
+}
