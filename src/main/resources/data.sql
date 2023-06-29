96dc32bd010aa515b48ecb0613bd8b073253ee06
mandanakhademi <mandanakhademi88@gmail.com>
2023-06-29 10:29:07 +0100

    Fix the issue with not creating tables automatically

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 TRUNCATE TABLE device_wearer;
 
-INSERT INTO device_wearer (id, device_wearer_id, first_name, last_name, "type")
+INSERT INTO device_wearer (id, device_wearer_id, first_name, last_name, type)
 VALUES (1, '3fc55bb7-ba52-4854-be96-661f710328fc', 'John', 'Smith', 'Historical Case Centric'),
        (2, 'ba34370c-9bf7-44eb-943e-2a6566205f99', 'Jane', 'Doe', 'Historical User Centric'),
        (3, 'cdfd3e77-0015-40bf-b9b7-0b608a2826ca', 'Michael', 'Jordan', 'Historical Case Centric')
