fc01147e462a333e1d5f648546ad03f7b1adf319
mandanakhademi <mandanakhademi88@gmail.com>
2023-07-03 14:06:22 +0100

    Move convert string to date to helper

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/helpers/DateConverter.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/helpers/DateConverter.kt
@@ -1,5 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.helpers
 
+import java.text.DateFormat
+import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.util.*
@@ -8,5 +10,11 @@ import java.util.*
 class DateConverter {
   fun convertToDateViaInstant(dateToConvert: LocalDateTime): Date {
     return java.sql.Timestamp.valueOf(dateToConvert)
+  }
+
+  fun convertFromStringToDate(dateString: String): Date {
+    val df2: DateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    val date: Date = df2.parse(dateString)
+    return date
   }
 }
