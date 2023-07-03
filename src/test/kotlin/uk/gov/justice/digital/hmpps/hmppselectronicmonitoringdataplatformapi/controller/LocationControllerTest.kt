ab7d9422165639abfa3f7ac979e4c2bfb66aa367
dtzz81 <denise.turkova@madetech.com>
2023-07-03 15:48:32 +0100

    Added testing a service (calling right type of data for verify)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/controller/LocationControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/controller/LocationControllerTest.kt
@@ -214,7 +214,7 @@ class LocationControllerTest {
 
     confirmNoError(result, expected)
     Assertions.assertThat(result.body?.locations).isEqualTo(expected.body?.locations)
-    verify(locationService, times(1)).getAllLocationsByDeviceWearerIdAndTimeFrame(any(), any(), any())
+    verify(locationService, times(1)).getAllLocationsByDeviceWearerIdAndTimeFrame(any<String>(), any<Date>(), any<Date>())
   }
 
   @Test
@@ -231,6 +231,32 @@ class LocationControllerTest {
     Assertions.assertThat(result.body?.error).isEqualTo(expected.body.error)
   }
 
+  @Test
+  fun `getLocationsByDeviceWearerIdAndTimeFrame should return list of two locations when receive valid dates and id and data exist` () {
+    val deviceWearerId = "b537065a-094e-47eb-8fab-9698a9664d35"
+    val startDate = "2000-10-31T01:30:07.000-05:00"
+    val endDate = "2000-10-31T01:30:20.000-05:00"
+    val device = Device(
+      id = 1,
+      deviceId = "deviceId",
+      modelId = "modelId",
+      firmwareVersion = "firmwareVersion",
+      deviceType = "deviceType",
+      status = "status",
+      batteryLifeRemaining = 20,
+    )
 
-  // test when we are calling service
+    val locationDataList: List<Location> = listOf(Location(1, device, 20.0, 20.0), Location(1, device, 30.0, 30.0))
+    val start: Date = DateConverter().convertFromStringToDate(startDate)
+    val end: Date = DateConverter().convertFromStringToDate(endDate)
+
+    Mockito.`when`(locationService.getAllLocationsByDeviceWearerIdAndTimeFrame(deviceWearerId, start, end)).thenReturn(locationDataList)
+
+    val expected: ResponseEntity<LocationResponse> = ResponseEntity(LocationResponse(locationDataList), HttpStatus.OK)
+    val result = LocationController(locationService).getLocationsByDeviceWearerIdAndTimeFrame(deviceWearerId, startDate, endDate)
+
+    confirmNoError(result, expected)
+    Assertions.assertThat(result.body?.locations).isEqualTo(expected.body?.locations)
+    verify(locationService, times(1)).getAllLocationsByDeviceWearerIdAndTimeFrame(any<String>(), any<Date>(), any<Date>())
+  }
 }
