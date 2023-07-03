00696846acc48a858585a409a5d385ddc913bdd2
dtzz81 <denise.turkova@madetech.com>
2023-07-03 16:56:48 +0100

    refactor location service based on emApiError - work in progress (need clarification location controller)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/controller/LocationControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/controller/LocationControllerTest.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.co
 
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.mockito.Mockito
 import org.mockito.kotlin.any
 import org.mockito.kotlin.times
@@ -10,6 +11,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.helpers.DateConverter
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.model.Device
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.model.EmApiError
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.model.Location
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.responses.LocationResponse
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.service.LocationService
@@ -19,10 +21,16 @@ class LocationControllerTest {
 
   val locationService = Mockito.mock(LocationService::class.java)
 
+  fun confirmException(result: EmApiError, expected: EmApiError) {
+    Assertions.assertThat(result.message).isEqualTo(expected.message)
+    Assertions.assertThat(result.status).isEqualTo(expected.status)
+    Assertions.assertThat(result.exceptionDetails).isEqualTo(expected.exceptionDetails)
+  }
   fun confirmNoError(result: ResponseEntity<LocationResponse>, expected: ResponseEntity<LocationResponse>) {
     Assertions.assertThat(result.body?.error).isEqualTo("")
     Assertions.assertThat(result.statusCode).isEqualTo(HttpStatus.OK)
   }
+
 
   @Test
   fun `getAllLocations Should return a list of all location data`() {
@@ -63,12 +71,10 @@ class LocationControllerTest {
   @Test
   fun `getAllLocations should return internal server error when there is an internal server issue`() {
     Mockito.`when`(locationService.getAllLocations()).thenThrow(RuntimeException("Exception"))
-    val expected: ResponseEntity<LocationResponse> =
-      ResponseEntity(LocationResponse("Something went wrong in our side"), HttpStatus.INTERNAL_SERVER_ERROR)
+    val expected = EmApiError("Something went wrong in our side", HttpStatus.INTERNAL_SERVER_ERROR)
 
-    val result = LocationController(locationService).getAllLocations()
-
-    Assertions.assertThat(result.body?.error).isEqualTo(expected.body.error)
+    val result = assertThrows<EmApiError> {  LocationController(locationService).getAllLocations() }
+    confirmException(result, expected)
   }
 
   @Test
@@ -98,13 +104,15 @@ class LocationControllerTest {
 
   @Test
   fun `getLocationsByDeviceWearerId should return bad request when it does not receive a valid id`() {
-    val userId = "456an"
-    val expected: ResponseEntity<LocationResponse> = ResponseEntity(LocationResponse("Insert a valid id"), HttpStatus.BAD_REQUEST)
+    val deviceWearerId = "456an"
+    val expected = EmApiError( "Insert a valid id", HttpStatus.BAD_REQUEST)
 
-    val result = LocationController(locationService).getLocationsDataByDeviceWearerId(userId)
+    val result = assertThrows<EmApiError> { LocationController(locationService).getLocationsDataByDeviceWearerId(deviceWearerId) }
 
-    Assertions.assertThat(result.statusCode).isEqualTo(expected.statusCode)
-    Assertions.assertThat(result.body?.error).isEqualTo(expected.body.error)
+    confirmException(result, expected)
+
+   //  Assertions.assertThat(result.statusCode).isEqualTo(expected.statusCode)
+   //  Assertions.assertThat(result.body?.error).isEqualTo(expected.body.error)
     verify(locationService, times(0)).getAllLocationsForDeviceWearer(any())
   }
 
@@ -121,16 +129,16 @@ class LocationControllerTest {
     Assertions.assertThat(result.body?.error).isEqualTo(expected.body.error)
   }
 
-  @Test
-  fun `getLocationsByDeviceWearerId should return internal server error when there is an internal server issue`() {
-    Mockito.`when`(locationService.getAllLocationsForDeviceWearer(any<String>())).thenThrow(RuntimeException("Exception"))
-    val expected: ResponseEntity<LocationResponse> =
-      ResponseEntity(LocationResponse("Something went wrong in our side"), HttpStatus.INTERNAL_SERVER_ERROR)
-    val userId = "b537065a-094e-47eb-8fab-9698a9664d35"
-    val result = LocationController(locationService).getLocationsDataByDeviceWearerId(userId)
-
-    Assertions.assertThat(result.body?.error).isEqualTo(expected.body.error)
-  }
+//  @Test
+//  fun `getLocationsByDeviceWearerId should return internal server error when there is an internal server issue`() {
+//    Mockito.`when`(locationService.getAllLocationsForDeviceWearer(any<String>())).thenThrow(RuntimeException("Exception"))
+//    val expected: ResponseEntity<LocationResponse> =
+//      ResponseEntity(LocationResponse("Something went wrong in our side"), HttpStatus.INTERNAL_SERVER_ERROR)
+//    val userId = "b537065a-094e-47eb-8fab-9698a9664d35"
+//    val result = LocationController(locationService).getLocationsDataByDeviceWearerId(userId)
+//
+//    Assertions.assertThat(result.body?.error).isEqualTo(expected.body.error)
+//  }
 
   @Test
   fun `getLocationsByDeviceWearerIdAndTimeFrame should return bad request when it does not receive valid deviceWearerId`() {
