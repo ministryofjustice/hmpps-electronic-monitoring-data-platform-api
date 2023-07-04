3c243019ec0fb9ed8e52f689568bb2c31cc0349a
dtzz81 <denise.turkova@madetech.com>
2023-07-04 16:46:46 +0100

    Fixed the issue with end date and refactored

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/controller/LocationController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/controller/LocationController.kt
@@ -31,39 +31,43 @@ class LocationController(@Autowired private val locationService: ILocationServic
 
   @GetMapping("/v1/device-wearer-id/{id}")
   fun getLocationsByDeviceWearerId(@PathVariable("id") deviceWearerId: String): ResponseEntity<LocationResponse> {
-      if (!StaticHelpers().validateUUID(deviceWearerId)) {
-        throw EmApiError("Insert a valid id", HttpStatus.BAD_REQUEST)
-      }
-      val result = locationService.getAllLocationsForDeviceWearer(deviceWearerId)
-      if (result.isEmpty()) {
-        return ResponseEntity(LocationResponse(message = "No data found"), HttpStatus.OK)
-      }
-      return ResponseEntity(LocationResponse(result), HttpStatus.OK)
+    if (!StaticHelpers().validateUUID(deviceWearerId)) {
+      throw EmApiError("Insert a valid id", HttpStatus.BAD_REQUEST)
+    }
+    val result = locationService.getAllLocationsForDeviceWearer(deviceWearerId)
+    if (result.isEmpty()) {
+      return ResponseEntity(LocationResponse(message = "No data found"), HttpStatus.OK)
+    }
+    return ResponseEntity(LocationResponse(result), HttpStatus.OK)
   }
 
-  @GetMapping("/v1/serach-by-time")
-  fun getLocationsByDeviceWearerIdAndTimeFrame(@RequestParam("deviceWearerId") deviceWearerId: String, @RequestParam("startDate") startDate: String, @RequestParam("endDate") endDate: String): ResponseEntity<LocationResponse> {
-      var errorMessage = ""
-      if (!StaticHelpers().validateUUID(deviceWearerId)) {
-        errorMessage = "Insert a valid device wearer id"
-      }
-      if (!StaticHelpers().isValidISODateTime(startDate)) {
-        errorMessage = "Insert a valid start date"
-      }
-      if (!StaticHelpers().isValidISODateTime(endDate)) {
-        errorMessage = "Insert a valid end date"
-      }
-      if (errorMessage != "") {
-        throw EmApiError(errorMessage, HttpStatus.BAD_REQUEST)
-      }
-      val start: Date = DateConverter().convertFromStringToDate(startDate)
-      val end: Date = DateConverter().convertFromStringToDate(endDate)
+  @GetMapping("/v1/search-by-time")
+  fun getLocationsByDeviceWearerIdAndTimeFrame(
+    @RequestParam("deviceWearerId") deviceWearerId: String,
+    @RequestParam("startDate") startDate: String,
+    @RequestParam("endDate") endDate: String,
+  ): ResponseEntity<LocationResponse> {
+    var errorMessage = ""
+    if (!StaticHelpers().validateUUID(deviceWearerId)) {
+      errorMessage = "Insert a valid device wearer id"
+    }
+    if (!StaticHelpers().isValidISODateTime(startDate)) {
+      errorMessage = "Insert a valid start date"
+    }
+    if (!StaticHelpers().isValidISODateTime(endDate)) {
+      errorMessage = "Insert a valid end date"
+    }
+    if (errorMessage != "") {
+      throw EmApiError(errorMessage, HttpStatus.BAD_REQUEST)
+    }
+    val start: Date = DateConverter().convertFromStringToDate(startDate)
+    val end: Date = DateConverter().convertFromStringToDate(endDate)
 
-      val result = locationService.getAllLocationsByDeviceWearerIdAndTimeFrame(deviceWearerId, start, end)
-      if (result.isEmpty()) {
-        return ResponseEntity(LocationResponse(message = "No data found"), HttpStatus.OK)
-      }
-      return ResponseEntity(LocationResponse(result), HttpStatus.OK)
+    val result = locationService.getAllLocationsByDeviceWearerIdAndTimeFrame(deviceWearerId, start, end)
+    if (result.isEmpty()) {
+      return ResponseEntity(LocationResponse(message = "No data found"), HttpStatus.OK)
+    }
+    return ResponseEntity(LocationResponse(result), HttpStatus.OK)
   }
 
 }
