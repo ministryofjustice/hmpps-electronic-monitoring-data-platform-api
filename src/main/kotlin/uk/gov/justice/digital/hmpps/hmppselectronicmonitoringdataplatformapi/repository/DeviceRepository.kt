2b122d52483ac85eaf37e4ee6d507bb77e8e0f01
dtzz81 <denise.turkova@madetech.com>
2023-07-05 14:31:16 +0100

    Added filtering all devices based on device wearer (created repository and service

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/repository/DeviceRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/repository/DeviceRepository.kt
@@ -1,10 +1,20 @@
 package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.repository
-
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.model.Device
 
 @Repository
 interface DeviceRepository : JpaRepository<Device, Int> {
+
+  @Query(
+    value = "SELECT DISTINCT d.*" +
+      "FROM device as d JOIN device_wearer as dw ON d.device_wearer_id = dw.id" +
+      "WHERE dw.device_wearer_id = deviceWearerId",
+
+    nativeQuery = true,
+  )
+
+  fun findDevicesByDeviceWearerId(deviceWearerId: String): List<Device>?
 
 }
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/service/DeviceService.kt
@@ -0,0 +1,26 @@
+package uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.model.Device
+import uk.gov.justice.digital.hmpps.hmppselectronicmonitoringdataplatformapi.repository.DeviceRepository
+
+interface IDeviceService {
+  fun getAllDevices(): List<Device>
+
+  fun getDevicesByDeviceWearerId(deviceWearerId: String): List<Device>
+
+}
+
+@Service
+class DeviceService(@Autowired private val deviceRepository: DeviceRepository) :
+  IDeviceService {
+  override fun getAllDevices(): List<Device> {
+    return deviceRepository.findAll().toList()
+  }
+
+  override fun getDevicesByDeviceWearerId(deviceWearerId: String): List<Device> {
+    return deviceRepository.findDevicesByDeviceWearerId(deviceWearerId) ?: listOf()
+  }
+
+}
