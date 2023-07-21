9fe9e664cffe7eb750400925c079d4b7a6379655
pedro.soares <pedro.soares@justice.gov.uk>
2023-07-21 14:04:56 +0100

    remove commented code

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/helpers/CSVHelper.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppselectronicmonitoringdataplatformapi/helpers/CSVHelper.kt
@@ -37,40 +37,4 @@ class CSVHelper {
       throw RuntimeException("fail to import data to CSV file: " + e.message)
     }
   }
-
-//  fun Convert(format: CSVFormat, rows: MutableList<List<String?>>): ByteArrayInputStream {
-//    try {
-//      ByteArrayOutputStream().use { out ->
-//        CSVPrinter(
-//          PrintWriter(out),
-//          format,
-//        ).use { csvPrinter ->
-//          for (row in rows) {
-//
-//            csvPrinter.printRecord(row)
-//          }
-//          csvPrinter.flush()
-//          return ByteArrayInputStream(out.toByteArray())
-//        }
-//      }
-//    } catch (e: IOException) {
-//      throw RuntimeException("fail to import data to CSV file: " + e.message)
-//    }
-//
-//  }
-
-  // fun LocationAggregationsToCSV(locations: List<LocationAggregation>): ByteArrayInputStream {
-//    val format = CSVFormat.DEFAULT.withHeader("Latitude", "Longitude", "DateTime")
-//    val rows: MutableList<List<String?>> = mutableListOf()
-//
-//    for (l in locations) {
-//
-//      val data: MutableList<String?> = mutableListOf()
-//      for (prop in l.getProperties()) {
-//        data.add(prop.get(l).toString())
-//      }
-//      rows.add(data)
-//    }
-//    return Convert(format, rows)
-//  }
 }
