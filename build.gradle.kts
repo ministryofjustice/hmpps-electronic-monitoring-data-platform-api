33b6243c72e58598948ec446bd74b2ea4688606c
jasper.jackson <jasper.jackson@madetech.com>
2024-02-13 12:40:09 +0000

    Upgraded all dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.0"
-  id("org.springframework.boot") version "3.1.0"
-  kotlin("plugin.spring") version "1.8.22"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.0"
+  id("org.springframework.boot") version "3.2.2"
+  kotlin("plugin.spring") version "1.9.22"
 }
 
 configurations {
@@ -19,27 +19,27 @@ repositories {
 }
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-webflux:3.0.4")
+  implementation("org.springframework.boot:spring-boot-starter-webflux:3.2.2")
   implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
   implementation("com.google.code.gson:gson:2.10.1")
-  implementation("org.springframework.boot:spring-boot-starter-hateoas:3.1.0")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
-  implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.20-RC")
-  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.20-RC")
-  implementation("com.microsoft.azure:applicationinsights-agent:3.4.9")
+  implementation("org.springframework.boot:spring-boot-starter-hateoas:3.2.2")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
+  implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.22")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.22")
+  implementation("com.microsoft.azure:applicationinsights-agent:3.4.19")
   implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
-  implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.1.0")
-  implementation("org.jetbrains.exposed:exposed-core:0.40.1")
-  implementation("org.jetbrains.exposed:exposed-dao:0.40.1")
-  implementation("org.jetbrains.exposed:exposed-jdbc:0.40.1")
-  implementation("org.postgresql:postgresql:42.5.4")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.2")
+  implementation("org.jetbrains.exposed:exposed-core:0.47.0")
+  implementation("org.jetbrains.exposed:exposed-dao:0.47.0")
+  implementation("org.jetbrains.exposed:exposed-jdbc:0.47.0")
+  implementation("org.postgresql:postgresql:42.7.1")
   implementation("org.apache.commons:commons-csv:1.10.0")
-  testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.0")
-  testImplementation("org.testcontainers:testcontainers:1.18.1")
-  testImplementation("org.testcontainers:junit-jupiter:1.18.1")
-  testImplementation("org.testcontainers:postgresql:1.18.1")
+  testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.2")
+  testImplementation("org.testcontainers:testcontainers:1.19.5")
+  testImplementation("org.testcontainers:junit-jupiter:1.19.5")
+  testImplementation("org.testcontainers:postgresql:1.19.5")
   api("me.paulschwarz:spring-dotenv:4.0.0")
-  developmentOnly("org.springframework.boot:spring-boot-devtools:3.1.0")
+  developmentOnly("org.springframework.boot:spring-boot-devtools:3.2.2")
 }
 
 java {
